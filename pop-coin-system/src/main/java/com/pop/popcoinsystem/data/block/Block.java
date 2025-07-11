2e172b35fd39a7dc855a9d374c0af99a616a2f8d
redstar <hecg@pingfang.net>
2025-07-11 22:53:41 +0800

    实现P2P网络

--- pop-coin-system/src/main/java/com/pop/popcoinsystem/data/block/Block.java
+++ pop-coin-system/src/main/java/com/pop/popcoinsystem/data/block/Block.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class Block {
 
-    private long index;//区块索引 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18.........................  在区块中的位置
+    private long index;//区块索引 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18.........................
     private String hash;//唯一的标识
 
 
