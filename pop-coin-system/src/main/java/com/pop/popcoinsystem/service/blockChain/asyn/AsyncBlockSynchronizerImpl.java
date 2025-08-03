959079c40749b38137502720ebe07d8acdf72381
redstar <hecg@pingfang.net>
2025-08-03 15:33:42 +0800

    添加测试

--- pop-coin-system/src/main/java/com/pop/popcoinsystem/service/blockChain/asyn/AsyncBlockSynchronizerImpl.java
+++ pop-coin-system/src/main/java/com/pop/popcoinsystem/service/blockChain/asyn/AsyncBlockSynchronizerImpl.java
@@ -377,6 +377,7 @@ public class AsyncBlockSynchronizerImpl implements AsyncBlockSynchronizer{
      * 拉取单批区块（带重试和超时）
      */
     private List<Block> fetchBlockBatch(NodeInfo remoteNode, byte[] startHash, byte[] endHash, int batchSize) throws InterruptedException {
+        log.info("拉取单批区块（带重试和超时）");
         for (int retry = 0; retry < MAX_RETRY; retry++) {
             try {
                 RpcProxyFactory proxyFactory = new RpcProxyFactory(kademliaNodeServer, remoteNode);
