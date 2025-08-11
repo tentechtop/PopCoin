6f9fadc7bd2054a870da018fb3b6c04eaeed46d0
redstar <hecg@pingfang.net>
2025-08-12 00:28:14 +0800

    完善3.0

--- pop-coin-system/src/main/java/com/pop/popcoinsystem/network/service/KademliaTcpHandler.java
+++ pop-coin-system/src/main/java/com/pop/popcoinsystem/network/service/KademliaTcpHandler.java
@@ -92,7 +92,7 @@ public class KademliaTcpHandler extends SimpleChannelInboundHandler<KademliaMess
             if (response != null) {
                 // 标记为响应消息
                 response.setResponse(true);
-                ctx.channel().writeAndFlush(response);
+                nodeServer.getTcpClient().sendMessage(response);//已经优化 会复用通道
             }
         } catch (Exception e) {
             log.error("处理请求消息 {} 时发生异常", message.getRequestId(), e);
